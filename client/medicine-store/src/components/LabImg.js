b8963a9f475fe6fe58ee6ae60d8f5343da7fe82c
Shivnath Adhya <shivnathadhya5@gmail.com>
2023-08-23 22:02:57 +0530

    shiv1

---
+++ client/medicine-store/src/components/LabImg.js
@@ -0,0 +1,11 @@
+import img from "../asset/640b5954a86b8381b4020998_Rectangle 31.webp"
+function LabImg() {
+  return (
+    <div>
+      
+      <img src={img} alt=""/>
+    </div>
+  )
+}
+
+export default LabImg;
